75d496771e8ec8570381bab34e118fa151871a9d
Nikos Kyknas <nkuknas@gmail.com>
2022-07-29 12:12:50 +0100

    Add docker compose shell

--- src/main/java/MoneyMakerApplication.java
+++ src/main/java/MoneyMakerApplication.java
@@ -8,7 +8,7 @@ public class MoneyMakerApplication {
     public static void main(String[] args) {
         Injector injector = Guice.createInjector(new HttpClientModule(), new KrakenModule());
         KrakenClient krakenClient = injector.getInstance(KrakenClient.class);
-//        System.out.println(krakenClient.getTickerInfo("XBTGBP").get().getResult().get("XXBTZGBP").getAskArray());
+        System.out.println(krakenClient.getTickerInfo("XBTGBP").get().getResult().get("XXBTZGBP").getAskArray());
         System.out.println(krakenClient.getBalance().get());
     }
 }
