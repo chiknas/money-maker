9c5c95561139b62afc084c180c66780c674a24e8
Nikos Kyknas <nkuknas@gmail.com>
2022-09-02 00:09:20 +0100

    open trades sql business logic was wrong

--- src/main/java/database/daos/TradeDao.java
+++ src/main/java/database/daos/TradeDao.java
@@ -16,7 +16,7 @@ public class TradeDao extends AbstractDao<TradeEntity> {
     public List<TradeEntity> findOpenTradesByStrategy(String strategyName) {
         String hql = "FROM TradeEntity t " +
                 "WHERE t.entryStrategy= :strategyName AND " +
-                "t.profit is not NULL";
+                "t.profit is NULL";
         Query query = entityManager.createQuery(hql);
         query.setParameter("strategyName", strategyName);
         return (List<TradeEntity>) query.getResultList();
